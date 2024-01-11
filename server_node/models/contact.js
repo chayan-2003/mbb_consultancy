3b7c614fb9cb2ea581f77b40594e541d5344b783
ShreyasiSen <shreyasisen05@gmail.com>
2024-01-11 22:05:11 +0530

    contactjs_changed

--- server_node/models/contact.js
+++ server_node/models/contact.js
@@ -7,9 +7,18 @@ const ContactSchema= new mongoose.Schema({
   email:{type:String,
     required:true},
 
-  contact_number:{type:Number,
-  required:true},
-
+  contact_number: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Custom validation function to check if the contact_number is a 10-digit number
+        const isValidNumber = /^\d{10}$/.test(value);
+        return isValidNumber;
+      },
+      message: 'Enter a 10-digit number',
+      },
+    },
   work_Title:{type:String,
     required:true},
 
