c2343d2f23a4ead20f89225f7b72924e4f0b8398
chandanbanerjee10 <97583515+chandanbanerjee10@users.noreply.github.com>
2024-01-22 22:30:02 +0530

    hamburger added (#4)

--- client_react/src/components/navbar.jsx
+++ client_react/src/components/navbar.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isMenuBarOpen, setIsMenuBarOpen] = useState(false);
+  const isMobile = window.innerWidth <= 640;
+
+  const toggleMenu = () => {
+    setIsMenuBarOpen(!isMenuBarOpen);
+  };
+
   return (
     <>
-      <nav className="bg-gradient-to-r from-slate-400 to-black p-4 flex flex-col sm:flex-row justify-between items-center border-b-4 border-white h-auto sm:h-24">
+      <nav className="bg-gradient-to-r from-slate-400 to-black p-4 flex flex-col sm:items-center sm:flex-row sm:justify-between border-b-4 border-white h-auto sm:h-24">
         <Link to="/">
-          <div className="text-white text-2xl sm:text-3xl font-bold">MBB CONSULTANCY</div>
+          <div className="text-white text-2xl sm:text-3xl font-bold">MBB Consultancy</div>
         </Link>
-        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+        {isMobile && (
+          <i className="fa fa-bars absolute right-3 cursor-pointer" style={{ fontSize: "36px", color: "white" }} onClick={toggleMenu}></i>
+        )}
+        
+        <div className={`flex flex-col mt-4 sm:mt-0 sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-3 ${isMobile ? (isMenuBarOpen ? 'block' : 'hidden') : 'block'}`}>
           <Link to="/contacts">
             <a
               href="#"
